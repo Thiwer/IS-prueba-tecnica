065275a8d87c6d009d9ebcd54a9257f65eef675b
Thiwer <danirivgar@gmail.com>
2020-11-03 19:55:29 +0100

    Domain First Approach

---
+++ IS-prueba-tecnica.Domain/Entities/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS_prueba_tecnica.Domain.Entities
+{
+    public class Vehicle
+    {
+        public string Matricula { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public string DniOwner { get; set; }
+
+        public Driver Driver { get; set; }
+
+        public ICollection<Driver> Drivers { get; private set; }
+
+        public Vehicle()
+        {
+            Drivers = new HashSet<Driver>();
+        }
+    }
+}
